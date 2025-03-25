f067e364c59ca19f8ef40b4bd123b280e79dbaf1
rina <k@rina.fyi>
2025-03-25 15:09:56 +1000

    finished

--- main.tf
+++ main.tf
@@ -17,3 +17,41 @@ provider "aws" {
     }
   }
 }
+
+resource "aws_instance" "hextris-server" {
+  ami = "ami-08b5b3a93ed654d19"
+  instance_type = "t2.micro"
+  key_name = "vockey"
+  tags = {
+    Name = "hextris"
+  }
+  user_data = file("./serve-hextris.sh")
+  security_groups = [aws_security_group.hextris-server.name]
+}
+
+output "hextris-url" {
+  value = aws_instance.hextris-server.public_ip
+}
+
+resource "aws_security_group" "hextris-server" {
+  name = "hextris-server"
+  description = "Hextris HTTP and SSH access"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
